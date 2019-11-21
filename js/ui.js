3c2e24308605d2e54476e99a50ec8b11e1a4a058
lucas-gomess <55445140+lucas-gomess@users.noreply.github.com>
2019-11-21 08:51:14 -0300

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,36 @@
-	document.addEventListener('DOMContentLoaded', function() {
+// essa funcao seleciona o row (que esta no index)
+// para depois desenhar todos os cards la
+const atividades = document.querySelector('.atividades');
+
+document.addEventListener('DOMContentLoaded', function() {
+	// menus laterais 
     var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
-  });
+    var instances = M.Sidenav.init(elems, {edge:'left'});
+
+// formulario para adicionar sobremesa
+	const forms = document.querySelectorAll('.side-form');
+	var instances2 = M.Sidenav.init(forms, {edge:'right'});
+});
+
+//funcao para desenhar o card da receita na tela
+//id, nome, descricao, link, endereco_imagem
+const desenhaCard = (data, id) => {
+
+    const html = `
+    <div class="col s12 m7 atividade" data="${id}">
+      <div class="card blue darken-4">
+        <div class="card-image atividade-imagem">
+        <img src="images/${data.endereco_imagem}">
+          <span class="card-title atividade-titulo">"${data.disciplina}"</span>
+        </div>
+        <div class="card-content">
+         <font color="white"><p>Calendário</p></font>
+        </div>
+        <div class="card-action  blue darken-4">
+          <a href="https://www.calendarioonline.com.br/calendario-2019/">Ver o Calendário</a>
+        </div>
+      </div>
+    </div>
+    sobremesas.innerHTML += html;
+
+};
